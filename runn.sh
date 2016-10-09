fad332f6e849da0b069390a42a6bd2410a906c5f
janek_g <jmgrzesik@gmail.com>
2016-10-09 11:43:46 +0200

    Adds new runn script

--- runn.sh
+++ runn.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-nasm -felf64 "$1".asm -o "$1".o && ld -m elf_x86_64  "$1".o -o "$1" && ./"$1";
-rm "$1".o;
-rm "$1";
+nasm -felf64 "$1".asm -o "$1".o && ld -m elf_x86_64  "$1".o -o "$1" && ./"$1" && rm "$1".o && rm "$1";
